9a4536c81c94edb4d7130c5ad770a75af7c170c8
jorenham <jhammudoglu@gmail.com>
2022-01-12 02:44:23 +0100

    build script for offline html

---
+++ build.py
@@ -0,0 +1,31 @@
+import pathlib
+
+from fastapi.testclient import TestClient
+
+from main import app
+
+HTML_DIR = pathlib.Path('./html')
+URL_PATHS = '/', '/jaxlib/'
+
+
+client = TestClient(app)
+
+
+def build_index(html_dir: pathlib.Path):
+    html_dir.mkdir(exist_ok=True)
+
+    for url_path in URL_PATHS:
+        response = client.get(url_path)
+        response.raise_for_status()
+
+        html_path = html_dir / url_path.removeprefix('/')
+        if not url_path.endswith('.html'):
+            html_path.mkdir(parents=True, exist_ok=True)
+            html_path /= 'index.html'
+
+        print(f'{url_path} -> {html_path}')
+        html_path.write_bytes(response.content)
+
+
+if __name__ == '__main__':
+    build_index(HTML_DIR)
